b9257a0c643b26bc322c0e5d6850ccdc041ed4ed
Chance Hudson <jchancehud@gmail.com>
2018-07-17 23:00:11 -0500

    Add basic index and flowconfig

---
+++ index.js
@@ -0,0 +1,20 @@
+// @flow
+
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  // Ignore other bots
+  if (message.author.bot) return;
+
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+bot.login(process.env.CLIENT_SECRET);
