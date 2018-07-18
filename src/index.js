8c117fb45c40ff5bbd429042bf41dcef8ac14f08
Chance Hudson <jchancehud@gmail.com>
2018-07-18 00:25:53 -0500

    Add address verification logic

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const invariant = require('invariant');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://192.168.1.200:8545'));
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
+const registerRegex = /register ([a-zA-Z0-9]*)/;
 
 bot.on('message', (msg: Discord.Message) => {
   // Ignore other bots
@@ -16,6 +19,25 @@ bot.on('message', (msg: Discord.Message) => {
 
   if (msg.content === 'ping') {
     return msg.reply('pong');
+  }
+
+  if (msg.content === 'help') {
+    return msg.reply(`
+      Usage:
+        register [address] - registers an ethereum address to your username
+    `);
+  }
+
+  if (registerRegex.test(msg.content)) {
+    const match = msg.content.match(registerRegex);
+    if (match === null) return;
+    invariant(match, 'matched address is null or undefined');
+    const address = match[1];
+    if (web3.utils.isAddress(address)) {
+      return msg.reply(`Address ${address} verified`);
+    } else {
+      return msg.reply(`${address} is not a valid ethereum address`);
+    }
   }
 });
 
